7c87700af37d243a75823dc094dd27c2c6adde9f
indaleci <indaleci4@gmail.com>
2024-04-30 04:29:09 +0200

    first commit

---
+++ Wordle-Terminal/src/Main.java
@@ -0,0 +1,17 @@
+// Scanner, substring, loops ANSI color codes
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("WORDLE!");
+
+        DictionaryWordle dictionaryWordle = new DictionaryWordle();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Wordle!");
+        int tries = 0;
+        int maxTries = 6;
+        String secretWord = DictionaryWordle.getRandomWord();
+
+    }
+}
